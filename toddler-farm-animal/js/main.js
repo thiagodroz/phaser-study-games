50718a8c87a97393c529cd9cafc89dc60b4e757d
thiagodroz <thiago.droz@gmail.com>
2017-08-31 23:48:50 -0300

    Initial sprites

--- toddler-farm-animal/js/main.js
+++ toddler-farm-animal/js/main.js
@@ -1,22 +1,25 @@
-//this game will have only 1 state
+var game = new Phaser.Game(640, 360, Phaser.AUTO);
+
 var GameState = {
-  //load the game assets before the game starts
   preload: function() {
     this.load.image('background', 'assets/images/background.png');
-    
+    this.load.image('chicken', 'assets/images/chicken.png');
+    this.load.image('horse', 'assets/images/horse.png');
+    this.load.image('pig', 'assets/images/pig.png');
+    this.load.image('sheep', 'assets/images/sheep.png');
   },
-  //executed after everything is loaded
   create: function() {
+    this.background = this.game.add.sprite(0, 0, 'background');
+    
+    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
+    this.chicken.anchor.setTo(0.5);
+    
+    this.chicken.scale.setTo(1.5);
   },
-  //this is executed multiple times per second
   update: function() {
-  },
-  
 
+  }
 };
-
-//initiate the Phaser framework
-var game = new Phaser.Game(640, 360, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
 game.state.start('GameState');
