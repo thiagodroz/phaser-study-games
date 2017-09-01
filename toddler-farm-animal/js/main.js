c0ff53c1a9b38d1be60b8c84c6ef0eb615adcea0
thiagodroz <thiago.droz@gmail.com>
2017-09-01 18:36:31 -0300

    Creating arrows and click events

--- toddler-farm-animal/js/main.js
+++ toddler-farm-animal/js/main.js
@@ -1,25 +1,56 @@
-var game = new Phaser.Game(640, 360, Phaser.AUTO);
-
 var GameState = {
   preload: function() {
-    this.load.image('background', 'assets/images/background.png');
-    this.load.image('chicken', 'assets/images/chicken.png');
-    this.load.image('horse', 'assets/images/horse.png');
-    this.load.image('pig', 'assets/images/pig.png');
-    this.load.image('sheep', 'assets/images/sheep.png');
+    this.game.load.image('background', 'assets/images/background.png');
+    this.game.load.image('arrow', 'assets/images/arrow.png');
+    this.game.load.image('chicken', 'assets/images/chicken.png');
+    this.game.load.image('horse', 'assets/images/horse.png');
+    this.game.load.image('pig', 'assets/images/pig.png');
+    this.game.load.image('sheep', 'assets/images/sheep.png');
+    
   },
   create: function() {
-    this.background = this.game.add.sprite(0, 0, 'background');
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     
-    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
-    this.chicken.anchor.setTo(0.5);
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
     
-    this.chicken.scale.setTo(1.5);
+    this.background = this.game.add.sprite(0, 0, 'background')
+    
+    this.pig = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'pig');
+    this.pig.anchor.setTo(0.5);
+
+    this.pig.inputEnabled = true;
+    this.pig.input.pixelPerfectClick = true;
+    this.pig.events.onInputDown.add(this.animateAnimal, this);
+
+    this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
+    this.leftArrow.anchor.setTo(0.5);
+    this.leftArrow.scale.x = -1;
+    this.leftArrow.customParams = {direction: -1};
+
+    this.leftArrow.inputEnabled = true;
+    this.leftArrow.input.pixelPerfectClick = true;
+    this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
+
+    this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
+    this.rightArrow.anchor.setTo(0.5);
+    this.rightArrow.customParams = {direction: 1};
+
+    this.rightArrow.inputEnabled = true;
+    this.rightArrow.input.pixelPerfectClick = true;
+    this.rightArrow.events.onInputDown.add(this.switchAnimal, this);    
   },
   update: function() {
-
+  },
+  switchAnimal: function(sprite, event) {
+    console.log('move animal');
+  },
+  animateAnimal: function(sprite, event) {
+    console.log('animate animal');
   }
 };
+
+var game = new Phaser.Game(640, 360, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
 game.state.start('GameState');
